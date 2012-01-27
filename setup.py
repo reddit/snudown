09f45ce9a54e555645aea9fbd6088a3b5c03e2c7
Neil Williams <neil@reddit.com>
2012-01-26 18:27:19 -0800

    Bump to version 1.0.4.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def c_files_in(directory):
 
 setup(
     name='snudown',
-    version='1.0.3',
+    version='1.0.4',
     author='Vicent Marti',
     author_email='vicent@github.com',
     license='MIT',

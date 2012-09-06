fba2891380a86858b6b3793b661c680c86d1fb79
Andre d <andre@andred.ca>
2012-09-05 23:35:05 -0700

    Add wiki formatting variant.

--- snudown.c
+++ snudown.c
@@ -7,36 +7,64 @@
 
 #define SNUDOWN_VERSION "1.0.6"
 
+enum snudown_renderer_mode {
+	RENDERER_USERTEXT = 0,
+	RENDERER_WIKI,
+	RENDERER_COUNT
+};
+
 struct snudown_renderopt {
 	struct html_renderopt html;
 	int nofollow;
 	const char *target;
 };
 
-static struct module_state {
+struct snudown_renderer {
+	struct sd_markdown* main_renderer;
+	struct sd_markdown* toc_renderer;
+	struct module_state* state;
+	struct module_state* toc_state;
+};
+
+struct module_state {
 	struct sd_callbacks callbacks;
 	struct snudown_renderopt options;
-} _state;
+};
 
-static struct sd_markdown* sundown = NULL;
+static struct snudown_renderer sundown[RENDERER_COUNT];
+
+static char* html_element_whitelist[] = { "tr", "th", "td", "table", "tbody", "thead", "tfoot", "caption", NULL };
+static char* html_attr_whitelist[] = {"colspan", "rowspan", "cellspacing", "cellpadding", "scope", NULL};
+
+static struct module_state usertext_toc_state;
+static struct module_state wiki_toc_state;
+static struct module_state usertext_state;
+static struct module_state wiki_state;
 
 /* The module doc strings */
 PyDoc_STRVAR(snudown_module__doc__, "When does the narwhal bacon? At Sundown.");
 PyDoc_STRVAR(snudown_md__doc__, "Render a Markdown document");
 
-static const int snudown_md_flags =
+static const unsigned int snudown_default_md_flags =
 	MKDEXT_NO_INTRA_EMPHASIS |
 	MKDEXT_SUPERSCRIPT |
 	MKDEXT_AUTOLINK |
 	MKDEXT_STRIKETHROUGH |
 	MKDEXT_TABLES;
 
-static const int snudown_render_flags = 
+static const unsigned int snudown_default_render_flags = 
 	HTML_SKIP_HTML |
 	HTML_SKIP_IMAGES |
 	HTML_SAFELINK |
-    HTML_ESCAPE |
-    HTML_USE_XHTML;
+	HTML_ESCAPE |
+	HTML_USE_XHTML;
+
+static const unsigned int snudown_wiki_render_flags = 
+	HTML_SKIP_HTML |
+	HTML_SAFELINK |
+	HTML_ALLOW_ELEMENT_WHITELIST |
+	HTML_ESCAPE |
+	HTML_USE_XHTML;
 
 static void
 snudown_link_attr(struct buf *ob, const struct buf *link, void *opaque)
@@ -53,29 +81,104 @@ snudown_link_attr(struct buf *ob, const struct buf *link, void *opaque)
 	}
 }
 
+static struct sd_markdown* custom_render(struct module_state* state,
+											const unsigned int renderflags,
+											const unsigned int markdownflags,
+											int toc_renderer) {
+	if(toc_renderer) {
+		sdhtml_toc_renderer(&state->callbacks,
+			(struct html_renderopt *)&state->options);
+	} else {
+		sdhtml_renderer(&state->callbacks,
+			(struct html_renderopt *)&state->options,
+			renderflags);
+	}
+	
+	state->options.html.link_attributes = &snudown_link_attr;
+	state->options.html.html_element_whitelist = html_element_whitelist;
+	state->options.html.html_attr_whitelist = html_attr_whitelist;
+	
+	return sd_markdown_new(
+		markdownflags,
+		16,
+		&state->callbacks,
+		&state->options
+	);
+}
+
+void init_default_render(PyObject *module) {
+	PyModule_AddIntConstant(module, "RENDERER_USERTEXT", RENDERER_USERTEXT);
+	sundown[RENDERER_USERTEXT].main_renderer = custom_render(&usertext_state, snudown_default_render_flags, snudown_default_md_flags, 0);
+	sundown[RENDERER_USERTEXT].toc_renderer = custom_render(&usertext_toc_state, snudown_default_render_flags, snudown_default_md_flags, 1);
+	sundown[RENDERER_USERTEXT].state = &usertext_state;
+	sundown[RENDERER_USERTEXT].toc_state = &usertext_toc_state;
+}
+
+void init_wiki_render(PyObject *module) {
+	PyModule_AddIntConstant(module, "RENDERER_WIKI", RENDERER_WIKI);
+	sundown[RENDERER_WIKI].main_renderer = custom_render(&wiki_state, snudown_wiki_render_flags, snudown_default_md_flags, 0);
+	sundown[RENDERER_WIKI].toc_renderer = custom_render(&wiki_toc_state, snudown_wiki_render_flags, snudown_default_md_flags, 1);
+	sundown[RENDERER_WIKI].state = &wiki_state;
+	sundown[RENDERER_WIKI].toc_state = &wiki_toc_state;
+}
+
 static PyObject *
 snudown_md(PyObject *self, PyObject *args, PyObject *kwargs)
 {
-	static char *kwlist[] = {"text", "nofollow", "target", NULL};
+	static char *kwlist[] = {"text", "nofollow", "target", "toc_id_prefix", "renderer", "enable_toc", NULL};
 
 	struct buf ib, *ob;
 	PyObject *py_result;
 	const char* result_text;
+	int renderer = RENDERER_USERTEXT;
+	int enable_toc = 0;
+	struct snudown_renderer _snudown;
+	int nofollow = 0;
+	char* target = NULL;
+	char* toc_id_prefix = NULL;
+	unsigned int flags;
 
 	memset(&ib, 0x0, sizeof(struct buf));
 
 	/* Parse arguments */
-	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s#|iz", kwlist,
-				&ib.data, &ib.size, &_state.options.nofollow, &_state.options.target)) {
+	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s#|izzii", kwlist,
+				&ib.data, &ib.size, &nofollow,
+				&target, &toc_id_prefix, &renderer, &enable_toc)) {
 		return NULL;
 	}
-
+	
+	if(renderer < 0 || renderer >= RENDERER_COUNT) {
+		PyErr_SetString(PyExc_ValueError, "Invalid renderer");
+		return NULL;
+	}
+	
+	_snudown = sundown[renderer];
+	
+	struct snudown_renderopt *options = &(_snudown.state->options);
+	options->nofollow = nofollow;
+	options->target = target;
+	
 	/* Output buffer */
 	ob = bufnew(128);
-
+	
+	flags = options->html.flags;
+	
+	if (enable_toc) {
+		_snudown.toc_state->options.html.toc_id_prefix = toc_id_prefix;
+		sd_markdown_render(ob, ib.data, ib.size, _snudown.toc_renderer);
+		_snudown.toc_state->options.html.toc_id_prefix = NULL;
+		
+		options->html.flags |= HTML_TOC;
+	}
+	
+	options->html.toc_id_prefix = toc_id_prefix;
+	
 	/* do the magic */
-	sd_markdown_render(ob, ib.data, ib.size, sundown);
-
+	sd_markdown_render(ob, ib.data, ib.size, _snudown.main_renderer);
+	
+	options->html.toc_id_prefix = NULL;
+	options->html.flags = flags;
+	
 	/* make a Python string */
 	result_text = "";
 	if (ob->data)
@@ -99,22 +202,10 @@ PyMODINIT_FUNC initsnudown(void)
 	module = Py_InitModule3("snudown", snudown_methods, snudown_module__doc__);
 	if (module == NULL)
 		return;
-
-	/* initialize the html renderer */
-	sdhtml_renderer(&_state.callbacks,
-			(struct html_renderopt *)&_state.options,
-			snudown_render_flags);
-
-	_state.options.html.link_attributes = &snudown_link_attr;
-
-	/* initialize the markdown parser */
-	sundown = sd_markdown_new(
-		snudown_md_flags,
-		16,
-		&_state.callbacks,
-		&_state.options
-	);
-
+	
+	init_default_render(module);
+	init_wiki_render(module);
+	
 	/* Version */
 	PyModule_AddStringConstant(module, "__version__", SNUDOWN_VERSION);
 }

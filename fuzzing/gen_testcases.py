c9714b839e4153f38cd20f7ce61ff3e80872bd78
Jordan Milne <jordan.milne@saynotolinux.com>
2015-06-01 12:59:49 -0700

    More sensible defaults for fuzzing

--- fuzzing/gen_testcases.py
+++ fuzzing/gen_testcases.py
@@ -12,6 +12,9 @@ import test_snudown
 
 cases = itertools.chain(test_snudown.cases.keys(), test_snudown.wiki_cases.keys())
 for i, md in enumerate(cases):
+    # skip huge testcases
+    if len(md) > 2048:
+        continue
     test_path = os.path.join('testing', 'testcases', 'test_default_%d.md' % i)
     with open(test_path, 'w') as f:
         f.write(md)
